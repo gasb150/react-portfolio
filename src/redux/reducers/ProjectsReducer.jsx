c701b3a7deb458fa6ab253d16889ccf75023029b
tavo <gasb_150@hotmail.com>
2021-06-16 00:51:49 -0500

    Create ProjectsReducer

--- src/redux/reducers/ProjectsReducer.jsx
+++ src/redux/reducers/ProjectsReducer.jsx
@@ -3,3 +3,37 @@ import {
   FETCH_PROJECTS_SUCCESS,
   FETCH_PROJECTS_FAILURE,
 } from '../actions/actionsTypes'
+
+const INITIAL_STATE = {
+  projects: [],
+  loading: false,
+  error: null,
+};
+
+
+const countryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_PROJECTS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_PROJECTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        projects: action.payload,
+      };
+    case FETCH_PROJECTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default countryReducer;
