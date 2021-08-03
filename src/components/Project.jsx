ba7c652132a2fac0f7e364ec75c6e3b38a02670a
gasb150 <gasb_150@hotmail.com>
2021-08-03 10:01:14 -0500

    Fix project display

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
-const Project = ({title, projects}) => {
-  //console.log(projects)
- 
+const Project = ({ project }) => {
   
- 
-  return(
+
+ const {images, name} = project
+
+const alt = `image_${name}`
+
+  return (
     <>
-  <div>Project here:{title}</div>
-  
-  </>
-)}
+
+      <div>Project Name: {name}</div>
+      {console.log(images)}
+      <div>llll {images.length}</div>
+      <div><img src={images[0]} alt={alt} /></div>
+      <div><img src={images[1]} alt={alt} /></div>
+      <div><img src={images[2]} alt={alt} /></div>
+      <div><img src={images[3]} alt={alt} /></div>
+      <div><img src={images[4]} alt={alt} /></div>
+
+    </>
+  )
+}
 
 export default Project
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,31 +1,36 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import portfolio from '../api/portfolio'
 import Project from '../components/Project'
 
 
 const Projects = () => {
   const [projects, setProjects] = useState([])
- 
 
-   
- 
+
   const onLoaded = async (data) => {
     const response = await portfolio.get('/proyects.json')
 
-    
-    
-    setProjects(response.data[0])
-    return response
-  }
-  onLoaded().then(data=>{
-    console.log(projects)
-    console.log(data)
-  })
- 
-  return (
 
-  <Project projects={projects} title={'Game style'}/>
-)}
+    setProjects(response.data)
+  }
+
+  useEffect(() => {
+    onLoaded()
+  }, [])
+
+  return (
+    <>
+  {console.log((projects))}
+        {projects.map(p=> 
+          <Project key={p.id} project={p} />
+        )}
+       
+      
+  
+      
+    </>
+  )
+}
 
 export default Projects
